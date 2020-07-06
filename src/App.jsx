b1e459e5a6840a760ac127c4b30560dd18d07a05
Sylvie <sylvie.z.lu@gmail.com>
2020-07-06 21:48:12 +0200

    fetch test ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return <div>Hello</div>;
-}
+const App = () => {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      const res = await axios('/api/places/');
+
+      setPlaces(res.data);
+    };
+    fetchPlaces();
+  }, []);
+
+  console.log(places);
+  return (
+    <div>
+      {places.map((place) => (
+        <div key={place.id}>{place.id}</div>
+      ))}
+      ;
+    </div>
+  );
+};
 
 export default App;
