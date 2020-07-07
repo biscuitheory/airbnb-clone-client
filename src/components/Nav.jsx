5b14fd2e60a037713e05ec2e707c176b2278aa43
Sylvie <sylvie.z.lu@gmail.com>
2020-07-08 00:59:39 +0200

    font-face & header mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import { ReactComponent as Icon0 } from '../assets/images/icons/svg0.svg';
+import { ReactComponent as Icon1 } from '../assets/images/icons/svg1.svg';
+import { ReactComponent as Icon2 } from '../assets/images/icons/svg2.svg';
+
 function Nav() {
   return (
     <nav className="nav">
       <div className="nav__links">
-        <span className="nav__link">Aide</span>
-        <span className="nav__link">Inscription</span>
+        <a className="nav__link" href="/">
+          <Icon0 className="nav__icon" />
+          <span>Explorer</span>
+        </a>
+        <a className="nav__link" href="/">
+          <Icon1 className="nav__icon" />
+          <span>Enregistrés</span>
+        </a>
+        <a className="nav__link" href="/">
+          <Icon2 className="nav__icon" />
+          <span>Connexion</span>
+        </a>
       </div>
     </nav>
   );
