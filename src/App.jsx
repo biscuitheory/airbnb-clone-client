4a4fcfbda9bdb6bf739b76949ee84bb8b7ab6217
My VO <kathy.nguyen198@gmail.com>
2020-07-14 14:43:57 +0200

    nav avec l'authentification

--- src/App.jsx
+++ src/App.jsx
@@ -6,56 +6,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
 
-<<<<<<< HEAD
-const initialState = {
-  isAuthenticated: false,
-  token: null,
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'LOGIN':
-      localStorage.setItem('token', action.payload.data.token);
-      return {
-        ...state,
-        isAuthenticated: true,
-        token: action.payload.data.token,
-      };
-    case 'LOGOUT':
-      localStorage.clear();
-      return {
-        ...state,
-        isAuthenticated: false,
-        token: null,
-      };
-    default:
-      return state;
-  }
-};
-
-const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  return (
-    <AuthContext.Provider
-      value={{
-        state,
-        dispatch,
-      }}
-    >
-      <Router>
-        <>
-          <Route exact path="/" component={Places} />
-          <Route exact path="/rooms/:id" component={Place} />
-          {/* <PrivateRoute exact path="/rooms/:id/booking" component={Booking} /> */}
-          <Route exact path="/bookings" component={Bookings} />
-          <Route exact path="/rooms/:id/booking" component={Booking} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-        </>
-      </Router>
-    </AuthContext.Provider>
-=======
 function App() {
   return (
     <div className="App">
@@ -65,7 +15,6 @@ function App() {
 
       <Footer />
     </div>
->>>>>>> a0b0ee9... refactoriser navbar
   );
 }
 
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import axios from 'axios';
 
 import { AuthContext } from '../../context/auth';
 import { Link } from 'react-router-dom';
