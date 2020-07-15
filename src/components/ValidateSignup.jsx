2e266cec8382ebeda0a1857de80948abe1f9f085
Sylvie <sylvie.z.lu@gmail.com>
2020-07-15 11:46:52 +0200

    error handling fix

--- src/components/ValidateSignup.jsx
+++ src/components/ValidateSignup.jsx
@@ -6,8 +6,9 @@ export default function validateSignup(values) {
     errors.first_name = 'Veuillez utiliser des caractères valides pour saisir votre prénom.';
   }
   if (!values.email) {
-  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
     errors.email = 'Une adresse e-mail est obligatoire.';
+  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+    errors.email = 'Veuillez insérer un e-mail valide.';
   }
   if (!values.password) {
     errors.password = 'Un mot de passe est obligatoire.';
