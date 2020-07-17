c0d09b35a4abea35dfc455b579c161aa3e4c635e
Sylvie <sylvie.z.lu@gmail.com>
2020-07-17 12:35:04 +0200

    place:id image fix

--- src/components/pages/Place.jsx
+++ src/components/pages/Place.jsx
@@ -27,13 +27,62 @@ const Place = () => {
   if (authState.isAuthenticated) {
     return (
       <>
-        <Link to="/">&#8249; Logements • Airbnb</Link>
-        <h1>{name}</h1>
-        <p>{city}</p>
-        <p>C&apos;est une perle rare.Les réservations sont fréquentes !</p>
-        <Link to={`/rooms/${id}/reserver`}>
-          <button type="button">Réserver</button>
-        </Link>
+        <Breakpoint small down>
+          <div className="place__header">
+            <Link to="/" style={{ textDecoration: 'none' }}>
+              &#8249; Logements • Airbnb
+            </Link>
+          </div>
+          <div className="place__container">
+            <div className="place__container-images">
+              <img className="place__container-image" src={photoPlace} alt="appartement" />
+            </div>
+            <div className="place__container-info">
+              <h1>{name}</h1>
+              <p className="place__container-info-city">{city}</p>
+              <p className="place__container-info-moreinfos">
+                {max_guests} pers. max ∙ {rooms} chambres ∙ {bathrooms} salles de bain
+              </p>
+              <p className="container-info-city-night"> {priceByNight}€/nuit </p>
+              <p className="container-info-city-month"> {priceByNight * 30}€ pour un mois </p>
+              <p className="place__container-info-more">
+                C&apos;est une perle rare. Les réservations sont fréquentes !
+              </p>
+            </div>
+          </div>
+          <div className="place__container-book">
+            <Link to="/login">
+              <button type="button" className="place__container-book-button">
+                Réserver
+              </button>
+            </Link>
+          </div>
+        </Breakpoint>
+        <Breakpoint medium up>
+          <div className="place__container">
+            <div className="place__container-images">
+              <img className="place__container-image" src={photoPlace} alt="appartement" />
+            </div>
+            <div className="place__container-info">
+              <h1>{name}</h1>
+              <p className="place__container-info-city">{city}</p>
+              <p className="place__container-info-moreinfos">
+                {' '}
+                {max_guests} pers. max ∙ {rooms} chambres ∙ {bathrooms} salles de bain
+              </p>
+              <p className="container-info-city-night"> {priceByNight}€/nuit </p>
+              <p className="container-info-city-month"> {priceByNight * 30}€ pour un mois </p>
+              <p className="place__container-info-more">
+                C&apos;est une perle rare. Les réservations sont fréquentes !
+              </p>
+              <Link to="/login">
+                <button type="button" className="place__container-book-button">
+                  Réserver
+                </button>
+              </Link>
+            </div>
+          </div>
+        </Breakpoint>
       </>
     );
   }
@@ -88,14 +137,12 @@ const Place = () => {
             <p className="place__container-info-more">
               C&apos;est une perle rare. Les réservations sont fréquentes !
             </p>
+            <Link to="/login">
+              <button type="button" className="place__container-book-button">
+                Réserver
+              </button>
+            </Link>
           </div>
-        </div>
-        <div className="place__container-book">
-          <Link to="/login">
-            <button type="button" className="place__container-book-button">
-              Réserver
-            </button>
-          </Link>
         </div>
       </Breakpoint>
     </>
